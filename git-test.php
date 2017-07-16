5388b33c14306f7732a871f612a13836c689979d
Ranta <kamranqb@gmail.com>
2017-07-16 14:50:48 +0500

    new file created git-test.php in test-1 branch

---
+++ git-test.php
@@ -0,0 +1,32 @@
+<?php
+
+class gitTesting{
+
+
+    public function abc(){
+        //method found in test-2 branch
+        echo 'method found in test-2 branch';
+    }
+
+    public function abcd(){
+        //method found in test 2 branch
+    }
+
+
+    public function abddsdfds(){
+
+    }
+
+    public function testOne(){
+        echo 'hmmm that cools';
+
+    }
+
+
+}
+
+
+
+
+
+?>
